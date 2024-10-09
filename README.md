cf0e80d129c55b13e858774460b504981441aedd
Ashton-Key <tashtonkey@yahoo.com>
2024-10-09 11:40:38 +0100

    Including setup.py

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 The github for my python methods to analyse Hermes-3 turbulence simulations. Built on the xhermes and boutdata/boututils frameworks, which in turn are build on xarray.
 
 turbhermes is the folder that contains the __init__.py, and will be the actual python module to be loaded.
+
+The setup.py should be able to allow a local install of the module, it has been mostly copied from the xhermes setup.py version.
---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+	return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+	name = "turbhermes",
+	version="0.1",
+	description="Analyse Hermes-3 simulations, extending from xhermes",
+	license="Apache",
+	long_description = read("README.md")
+	classifiers=[
+		"Development Status :: 2 - Pre-Alpha",
+		"Intended Audience :: Science/Research",
+		"Intended Audience :: Education",
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: Apache License",
+		"Natural Language :: English",
+		"Operating System :: POSIX :: Linux",
+		"Programming Language :: Python :: 3.6",
+		"Topic :: Scientific/Engineering :: Physics",
+	],
+	install_requires=["xarray", "xbout", "xhermes"],
+	packages=find_packages(),
+	include_package_data=True,
+)
