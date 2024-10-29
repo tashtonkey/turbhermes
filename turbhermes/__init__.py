c4b1a838308cda9141d2be45a308bf9d63623f5e
tashtonkey <tashtonkey@yahoo.com>
2024-10-29 17:33:53 +0000

    Edited the init to include all functions

--- turbhermes/__init__.py
+++ turbhermes/__init__.py
@@ -1,4 +1,4 @@
 import matplotlib.pyplot as plt
-from .plotting import multi_line_raw 
+from .plotting import multi_line_raw, multi_line_zeta 
 from .accessors import TurbulenceDataArrayAccessor, UtilityDatasetAccessor, TurbulenceDatasetAccessor
 
