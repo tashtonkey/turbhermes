c585021a3a940299496310342e05b7d4424361ea
tashtonkey <tashtonkey@yahoo.com>
2025-01-31 11:17:39 +0000

    Fixing plt issue

--- turbhermes/plotting.py
+++ turbhermes/plotting.py
@@ -1,4 +1,4 @@
-
+import matplotlib.pyplot as plt
 def multi_line_raw(bd_list, variable, coordinates, ax=None, **kwargs):
     """
     Plots a time trace of all the datasets in the bd_list, converted to correct units
