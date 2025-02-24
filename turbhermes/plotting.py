40810f5357df29da5b4553b3f2d88808941a1418
tashtonkey <tashtonkey@yahoo.com>
2025-02-24 16:00:25 +0000

    plot_vline -> axvline

--- turbhermes/plotting.py
+++ turbhermes/plotting.py
@@ -319,7 +319,7 @@ def diagonal_slice_plotting(
             ax.set_yscale("symlog", linthresh=linear_threshold)
 
     # Plot separatrix
-    ax.plot_vline(0, "--")
+    ax.axvline(0, "--")
 
     if animate:
         _add_controls(anim, controls, t_label)
