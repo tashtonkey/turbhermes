ffe58738cd1f164de6d19e2e082a59ffb1a6e86b
tashtonkey <tashtonkey@yahoo.com>
2025-11-28 14:21:37 +0000

    Updating init

--- turbhermes/__init__.py
+++ turbhermes/__init__.py
@@ -2,4 +2,4 @@ import matplotlib.pyplot as plt
 import xarray
 from .plotting import multi_line_raw, multi_line_zeta 
 from .accessors import TurbulenceDataArrayAccessor, UtilityDatasetAccessor, TurbulenceDatasetAccessor
-
+from .functions import *
